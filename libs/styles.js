438150b798159cd66bb871b8f82c5ded31b5bad6
sletuffe <sylvain@letuffe.org>
2025-09-23 11:49:07 +0200

    It is time to accept that MRI was a very crappy name for a TMS service's name, it does not tell you anything about what it really is, I switch back to his former name. I might even one day get the openhikingmap.org

--- libs/styles.js
+++ libs/styles.js
@@ -6,11 +6,11 @@ var base_url = [ window.location.protocol + '//' + window.location.hostname + '/
 //base styles (not transparent)
 var all_available_styles = [
   {
-    id: 'MRI',
-    name: 'Maps.Refuges.Info',
+    id: 'ohm',
+    name: 'OpenHikingMap',
     tile_urls: [ base_url[0] + 'hiking/${z}/${x}/${y}.png' ],
-    attribution: '<a href="https://wiki.openstreetmap.org/wiki/Hiking/mri">More informations &copy; sly</a> MODIS/Terra Snow Cover 8-day by NSDIC <br>ASTER GDEM is a product of METI and NASA<br> SRTM V4 thanks to CGIAR-CSI<br>EU-DEM produced using Copernicus data and information funded by the European Union.<br>',
-    description: 'MRI hiking rendering',
+    attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">More informations &copy; sly</a> MODIS/Terra Snow Cover 8-day by NSDIC <br>ASTER GDEM is a product of METI and NASA<br> SRTM V4 thanks to CGIAR-CSI<br>EU-DEM produced using Copernicus data and information funded by the European Union.<br>',
+    description: 'OpenHikingMap main rendering',
     numZoomLevels: 19
   },
   {
@@ -18,7 +18,7 @@ var all_available_styles = [
     name: 'OpenTopoMap local clone',
     tile_urls: [ base_url[0] + 'otm/${z}/${x}/${y}.png' ],
     attribution: '<a href="https://opentopomap.org/about">More informations &copy; OTM</a> MODIS/Terra Snow Cover 8-day by NSDIC <br>ASTER GDEM is a product of METI and NASA<br> SRTM V4 thanks to CGIAR-CSI<br>EU-DEM produced using Copernicus data and information funded by the European Union.',
-    description: 'OpenTopoMap customization',
+    description: 'OpenTopoMap local adaptation and customization',
     numZoomLevels: 19
   },
   {
@@ -26,7 +26,7 @@ var all_available_styles = [
     name: 'OpenTopoMap Official',
     tile_urls: [ 'https://a.tile.opentopomap.org/${z}/${x}/${y}.png' ],
     attribution: '<a href="https://opentopomap.org/about">More informations &copy; OTM</a> MODIS/Terra Snow Cover 8-day by NSDIC <br>ASTER GDEM is a product of METI and NASA<br> SRTM V4 thanks to CGIAR-CSI<br>EU-DEM produced using Copernicus data and information funded by the European Union.',
-    description: 'OpenTopoMap customization',
+    description: 'OpenTopoMap main server',
     numZoomLevels: 17
   }
 ];

49e89a3b48a8c84aeddff744d8cbd2c3e9a103c2
sletuffe <sylvain@letuffe.org>
2025-10-12 15:18:38 +0200

    Moving default https://maps.refuges.info to https://tile.openmaps.fr, plus renamming explicitly all styles to opentopomap and openhikingmap instead of ohm, otm or hiking + adding my apache vhost configurations

--- libs/styles.js
+++ libs/styles.js
@@ -1,28 +1,23 @@
-// liste des styles disponibles
-// Dans une vie postérieure, cette liste pourra être dynamiquement générée en interrogeant les serveurs de rendus et les styles dont ils disposent -- sly
-
-var base_url = [ window.location.protocol + '//' + window.location.hostname + '/' ];
-
-//base styles (not transparent)
+// liste of styles
 var all_available_styles = [
   {
-    id: 'ohm',
+    id: 'OpenHikingMap',
     name: 'OpenHikingMap',
-    tile_urls: [ base_url[0] + 'hiking/${z}/${x}/${y}.png' ],
-    attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">&copy; sly</a>',
+    tile_urls: [ 'https://tile.openmaps.fr/openhikingmap/${z}/${x}/${y}.png' ],
+    attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">&copy; OpenHikingMap</a>',
     description: 'OpenHikingMap main rendering',
     numZoomLevels: 19
   },
   {
-    id: 'otm',
+    id: 'OpenTopoMap',
     name: 'OpenTopoMap local clone',
-    tile_urls: [ base_url[0] + 'otm/${z}/${x}/${y}.png' ],
-    attribution: '<a href="https://github.com/RefugesInfo/OpenTopoMap">&copy; OTM-R</a>',
+    tile_urls: [ 'https://tile.openmaps.fr/opentopomap/${z}/${x}/${y}.png' ],
+    attribution: '<a href="https://github.com/sletuffe/OpenTopoMap">&copy; OTM-R</a>',
     description: 'OpenTopoMap local adaptation and customization',
     numZoomLevels: 17
   },
   {
-    id: 'official-otm',
+    id: 'official-OpenTopoMap',
     name: 'OpenTopoMap Official',
     tile_urls: [ 'https://a.tile.opentopomap.org/${z}/${x}/${y}.png' ],
     attribution: '<a href="https://opentopomap.org/about">&copy; OTM</a>',
